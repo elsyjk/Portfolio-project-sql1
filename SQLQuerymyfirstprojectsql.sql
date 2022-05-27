3a695549ca6d127727fc07f21e7a8b9f42960844
elsyjk <93622948+elsyjk@users.noreply.github.com>
2022-05-27 16:19:18 -0500

    Update SQLQuerymyfirstprojectsql.sql

--- SQLQuerymyfirstprojectsql.sql
+++ SQLQuerymyfirstprojectsql.sql
@@ -19,18 +19,18 @@ insert into Orders (OrderDate, CustomerID, ProductID) values (GetDate(), 2,2)
 Select sum(p.Price) as Total
 From Orders o
 inner join Products p on o.ProductID=p.id
-inner join Customer c on o. CustomerID=c.id
+inner join Customer c on o.CustomerID=c.id
 
 Select c.LastName,p.ProductName, sum(p.Price) as Total
 From Orders o
 inner join Products p on o.ProductID=p.id
-inner join Customer c on o. CustomerID=c.id
+inner join Customer c on o.CustomerID=c.id
 group by c.LastName,p.ProductName
 
 Select c.City,sum(p.Price) as Total,AVG(p.Price) as Averprice
 From Orders o
 inner join Products p on o.ProductID=p.id
-inner join Customer c on o. CustomerID=c.id
+inner join Customer c on o.CustomerID=c.id
 group by c.City
 
 
